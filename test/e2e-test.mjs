a382d63a1e539cc2e0076fb51f2d39a90c6c37bc
R. Douglas Barbieri <doug@dooglio.net>
2022-04-28 22:39:21 -0500

    chore: only bring in packages we need from octokit
    Also, add ncc compiler

--- test/e2e-test.mjs
+++ test/e2e-test.mjs
@@ -1,8 +1,16 @@
-import { Octokit } from 'octokit'
+import { Octokit } from '@octokit/rest'
+import { graphql } from '@octokit/graphql'
 import generateReleaseNotes from '../src/generate-release-notes.mjs'
 
 async function run() {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+  const octokit = {
+    rest: new Octokit({ auth: process.env.GITHUB_TOKEN }),
+    graphql: graphql.defaults({
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    }),
+  }
   await generateReleaseNotes(octokit, 'exodus-desktop')
 }
 
